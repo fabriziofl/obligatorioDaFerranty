777e52827e7b1621a3493e4d4822546c2f34c7dd
Fabrizio Ferranty <fabriziofl@gmail.com>
2025-10-28 00:11:27 -0300

    primero

---
+++ src/test/java/uy/edu/ort/obligatorioferranty/ObligatorioferrantyApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.ort.obligatorioferranty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioferrantyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
